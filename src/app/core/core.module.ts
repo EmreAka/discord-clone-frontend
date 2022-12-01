0817453586dd5e73b134334d308678d60037b67e
Emre AKA <aka548548@gmail.com>
2022-12-01 04:19:21 +0300

    sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class CoreModule { }
