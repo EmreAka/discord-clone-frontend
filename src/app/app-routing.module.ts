ae8b6fba170411f81f4f86df0efae73dd0b591b7
Emre AKA <aka548548@gmail.com>
2022-12-01 07:06:47 +0300

    server title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:"channels", loadChildren: () => import("./views/server-view/server-view.module").then(module => module.ServerViewModule)}
+  {
+    path: "channels/:id",
+    loadChildren: () => import("./views/server-view/server-view.module").then(module => module.ServerViewModule)
+  }
 ];
 
 @NgModule({
--- src/app/views/server-view/components/server-header/server-header.component.ts
+++ src/app/views/server-view/components/server-header/server-header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-header.component.scss']
 })
 export class ServerHeaderComponent {
-
+  serverTitle: string = "Emre'nin yeri"
 }
